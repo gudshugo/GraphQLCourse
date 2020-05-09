aad90188defd1b5b6d96e8928e7023e02a4f22d1
Hugo <gudshugo@gmail.com>
2020-05-09 14:21:52 -0300

    D - index.js - criando primeiro tipo (Usuario)

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,18 +1,49 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+    scalar Date
+
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # Pontos de entrada da API
     type Query {
-        ola: String
-        horaCerta: String
+        ola: String!
+        horaCerta: Date!
+        usuarioLogado: Usuario
     }
 `
 
 const resolvers = {
+
+    Usuario: {
+        salario (usuario){
+            return usuario.salario_real
+        }
+    },
+
     Query: {
+        ola(){
+            return 'Bom dia!'
+        },
         horaCerta (){
-            let dataAtual = new Date()
-            return dataAtual.toString()
+            return new Date
+        },
+        usuarioLogado(){
+            return {
+                id: 1,
+                nome: 'Hugo',
+                email: 'email@email.com',
+                idade: 24,
+                salario_real: 12.34,
+                vip: true
+            }
         }
     }
 }
