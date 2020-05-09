49df0dd00fa13cb99f80822ef745d1bf1529ed12
Hugo <gudshugo@gmail.com>
2020-05-09 18:04:34 -0300

    D - index.js - refatorando schemas

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { importSchema } = require('graphql-import')
 
 const usuarios = [
     {
@@ -34,45 +35,6 @@ const perfis = [
         nome: 'Administrador'
     }
 ]
-
-const typeDefs = gql`
-    scalar Date
-
-    type Produto {
-        nome: String!
-        preco: Float!
-        desconto: Float
-        precoComDesconto: Float
-    }
-    
-    type Usuario {
-        id: Int
-        nome: String!
-        email: String!
-        idade: Int
-        salario: Float
-        vip: Boolean
-        perfil: Perfil
-    }
-
-    type Perfil {
-        id: Int
-        nome: String!
-    }
-
-    # Pontos de entrada da API
-    type Query {
-        ola: String!
-        horaCerta: Date!
-        usuarioLogado: Usuario
-        produtoEmDestaque: Produto
-        numerosMegaSena: [Int!]!
-        usuarios: [Usuario]
-        usuario(id: Int): Usuario
-        perfis: [Perfil]
-        perfil(id: Int): Perfil
-    }
-`
 
 const resolvers = {
 
@@ -151,7 +113,7 @@ const resolvers = {
 }
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: importSchema('./schema/index.graphql'),
     resolvers
 })
 
