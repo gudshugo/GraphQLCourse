2ee8030eee164a530f658d5a852e522f6a0d3ebf
Hugo <gudshugo@gmail.com>
2020-05-09 13:20:03 -0300

    D - index.js - primeira query

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,11 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
-
+    # Pontos de entrada da API
+    type Query {
+        ola: String
+    }
 `
 
 const resolvers = {
-
+    Query: {
+        ola () {
+            return 'Bom dia'
+        }
+    }
 }
 
 const server = new ApolloServer({
