4261d6ff12c02f014144ad341a98947525b2c74a
Hugo <gudshugo@gmail.com>
2020-05-09 18:23:06 -0300

    D - index.js - criando enum de status do usuario

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -4,21 +4,24 @@ const usuarios = [
         nome: 'Hugo',
         email: 'email@email.com',
         idade: 24,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'ATIVO'
     },
     {
         id : 2,
         nome: 'Gilberto',
         email: 'email@email.com',
         idade: 21,
-        perfil_id: 2
+        perfil_id: 2,
+        status: 'INATIVO'
     },
     {
         id : 3,
         nome: 'Elizeu',
         email: 'email@email.com',
         idade: 25,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'BLOQUEADO'
     }
 ]
 
