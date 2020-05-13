3cb0df318f776ffecc56c21c481278c09462e648
gudshugo <gudshugo@gmail.com>
2020-05-12 21:44:47 -0300

    D - mutation - criando verificacao pra mutation de criacao e criando mutation de exclusao

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -2,17 +2,33 @@ const { usuarios, proximoId } = require('../data/db')
 
 
 module.exports = {
-  novoUsuario(_, { nome, email, idade }) {
+  //o operador ...(spread) itera os argumentos ({ nome, email, idade }) da função no objeto
+  novoUsuario(_, args) {
+    const emailExistente = usuarios
+      .some(u => u.email === args.email)
+
+    if (emailExistente) {
+      throw new Error('E-mail já cadastrado')
+    }
+
     const novo = {
       id: proximoId(),
-      nome,
-      email,
-      idade,
+      ...args,
       perfil_id: 1,
       status: 'ATIVO'
     }
 
     usuarios.push(novo)
     return novo
+  },
+  excluirUsuario(_, { id }) {
+    const i = usuarios
+      .findIndex(u => u.id === id)
+
+    if (i < 0) return null
+
+    const excluidos = usuarios.splice(i, 1)
+    return excluidos ? excluidos[0] : null
   }
+
 }
